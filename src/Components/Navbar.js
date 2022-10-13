9bde7a87479c0348d0e3e88606ba073a3bcad678
Ujjwal Tiwari <ujwltiwari494@gmail.com>
2022-10-13 16:09:08 +0530

    :fire: Movies App :sparkles:

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-light">
+      {/* <nav className="navbar navbar-expand-lg bg-light">
         <div className="container-fluid">
           <a className="navbar-brand lead" href="/">
             Movies App
@@ -78,6 +78,46 @@ const Navbar = () => {
                 aria-label="Search"
               />
               <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav> */}
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Movies App
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i class="fas fa-bars"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <a class="navbar-brand lead" href="/favourites">
+                Favourites
+              </a>
+            </ul>
+            <form class="d-flex input-group w-auto">
+              <input
+                type="search"
+                class="form-control"
+                placeholder="Type query"
+                aria-label="Search"
+              />
+              <button
+                class="btn btn-outline-primary"
+                type="button"
+                data-mdb-ripple-color="dark"
+              >
                 Search
               </button>
             </form>
